bc14d971060963010a524825c5a69e0c21c3eb63
christian-byrne <abolkonsky.rem@gmail.com>
2024-10-26 21:19:42 -0700

    Remove factor calculations in favor of LUT

---
+++ src/columnsort/scripts/create_factor_pair_lookup_table.py
@@ -0,0 +1,60 @@
+
+import itertools
+
+def find_factors_pairs(n):
+    result = {}
+
+    for num in range(1, n + 1):
+        # Generate factor pairs (p, s) for the current number
+        factors = [(p, s) for p in range(2, num + 1) if num % p == 0 for s in range(2, num + 1) if p * s == num]
+        
+        # Filter pairs based on conditions: p >= 2 * (s - 1)^2 and s divides p
+        filtered_factors = [
+            (p, s) for p, s in factors 
+            if p >= 2 * (s - 1) ** 2 and p % s == 0
+        ]
+        
+        # Sort by the first element (p) in ascending order
+        filtered_factors.sort(key=lambda x: x[0])
+        
+        # Select the first pair if available and add to result dictionary
+        if filtered_factors:
+            result[num] = filtered_factors[0]
+
+    return result
+
+
+TOTAL = 500
+x = find_factors_pairs(TOTAL)
+
+final = {
+    0: (1, 0, 0)
+}
+def find_closest_key(num, pairs):
+    closest = None
+    cur = None
+    for key in pairs:
+        
+        if key <= num:
+            distance = abs(key - num)
+            if closest is None or distance < closest:
+              closest = distance 
+              cur = key
+        
+    if cur is None:
+        final[num] = (1, num, 0)
+        return
+
+    final[num] = pairs[cur]
+    final[num] = (final[num][0], final[num][1], abs(closest))
+
+
+for i in range(1, TOTAL):
+    find_closest_key(i, x)
+
+
+# Format and print in a way that can be copy and pasted into a Java 2D array
+print("  private final int[][] LOOKUP_TABLE = {", end="")
+for key in final:
+    print(f"    {{ {final[key][0]}, {final[key][1]}, {final[key][2]} }},", end="")
+print("};", end="")
